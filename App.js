adb2ba137e1836955b0b67428627718b46cc6028
rezaBG <rezabarzegargashti@gmail.com>
2023-04-22 12:00:09 +0200

    init App.js

--- App.js
+++ App.js
@@ -1,2 +1,15 @@
 import React from "react";
-import Navbar from "";
+import Navbar from "./components/Navbar";
+
+import Main from "./components/main";
+
+const App = () => {
+  return (
+    <div className="countainer">
+      <Navbar />
+      <Main />
+    </div>
+  );
+};
+
+export default App;
