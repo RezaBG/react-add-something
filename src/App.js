079cae9d5ead56b3d884cfd9b4ced521ff09758e
Reza Barzergar Gashti <101470327+RezaBG@users.noreply.github.com>
2023-05-01 20:06:42 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
+import CourseGoalList from "./CourseGoals/CourseGoalList/CourseGoalList";
+import CourseInput from "./CourseGoals/CourseInput/CourseInput";
 import "./App.css";
 
-const App = () => {};
+const App = () => {
+  const [courseGoals, setCouseGoals] = useState([
+    { text: "Do all exercises!", id: "g1" },
+    { text: "Finish the course!", id: "g2" },
+  ]);
+
+  const addGoalHandler = (enteredText) => {
+    setCouseGoals((prevGoals) => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
+
+  const deleteItemHandler = (goalId) => {
+    setCouseGoals((prevGoals) => {
+      const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
+  let content = (
+    <p style={{ textAlign: "center" }}>No goals found. Maybe and one?</p>
+  );
+
+  if (courseGoals.length > 0) {
+    content = (
+      <CourseGoalList item={courseGoals} onDeleteItem={deleteItemHandler} />
+    );
+  }
+  return (
+    <div>
+      <section>
+        <CourseInput onAddGoal={addGoalHandler} />
+      </section>
+      <section id="goals">{content}</section>
+    </div>
+  );
+};
 
 export default App;
